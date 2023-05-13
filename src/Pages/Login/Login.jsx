b9d629bc94e8bdf53798875f6c0cf12103009f3b
najmul19977 <najmulkabir19977@gmail.com>
2023-05-13 12:52:52 +0600

    first commit

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,63 @@
+import React, { useContext } from 'react';
+import img from '../../assets/images/login/login.svg'
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Providers/AuthProvider';
+import { Result } from 'postcss';
+
+const Login = () => {
+    const {signIn} =useContext(AuthContext);
+    const handleLogin =event => {
+        event.preventDefault();
+        const form = event.target;
+        
+        const email = form.email.value;
+        const password = form.name.value;
+        console.log(email,password);
+        signIn(email,password)
+        .then(Result =>{
+            const user = Result.user;
+            console.log(user);
+        })
+        .catch(error =>console.log(error));
+    }
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row">
+                <div className="mr-14 w-1/2">
+
+                    <img src={img} alt="" />
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div className="card-body">
+                        <h1 className="text-5xl font-bold"> Please Login </h1>
+                        <form onSubmit={handleLogin}>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="text" name='email' placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="text" name='password' placeholder="password" className="input input-bordered" />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+
+                                <input className="btn btn-primary" type="button" value="Submit" />
+                            </div>
+                        </form>
+                        <p className='my-4'>New to Car Doctors <Link className='text-orange-600 font-bold' to='/signup'>Sign Up</Link></p>
+                    
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/Providers/AuthProvider.jsx
@@ -0,0 +1,47 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import app from '../firebase/firebase.confige';
+
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [loading,setLoding] = useState(true);
+
+    const createUser =(email,password) =>{
+        setLoding(true);
+
+        return createUserWithEmailAndPassword(auth,email,password);
+    }
+    const signIn = (email,password) =>{
+        setLoding(true);
+        return signInWithEmailAndPassword(auth,email,password);
+    }
+
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth,currentUser=>{
+            setUser(currentUser);
+            console.log('current user',currentUser);
+            setLoding(false);
+        });
+        return()=>{
+            return unsubscribe();
+        }
+    })
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        signIn
+
+    }
+    return (
+      <AuthContext.Provider value={authInfo}>
+        {children}
+
+      </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
